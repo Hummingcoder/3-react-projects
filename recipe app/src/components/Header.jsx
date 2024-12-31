4b20a35e6abcfccd8304181cd1ca6f350597bc31
Vishal D. <dwivedivishal0702@gmail.com>
2024-12-31 19:02:47 +0530

    food recipe  added

--- recipe app/src/components/Header.jsx
+++ recipe app/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className="w-full bg-white h-[60px]  p-6">
       <p className="text-xl font-semibold font-serif">Find Recipes</p>
+      <p>imdumb</p>
       <nav className="w-full  h-full flex items-center text-lg justify-end gap-5">
         <NavLink
           className={({ isActive }) =>
